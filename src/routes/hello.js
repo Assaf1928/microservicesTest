b9fd3a78fa08a319deb755f140d20c672be56764
Eagle1928 <assaf1928@gmail.com>
2020-10-09 14:06:30 +0300

    uploading

---
+++ src/routes/hello.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+router.route('/').get((req,res)=> {
+msg = `Hello ${req.query.name || "World"} from get request`;
+res.json({msg})
+});
+
+router.route('/:lang').get((req,res) => {
+    switch(req.params.lang) {
+        case "es": 
+            msg = 'Hola Mundo'
+            break;
+        
+        case "en": 
+            msg = 'Hello Mundo'
+            break;
+        
+        default:
+        msg = "Hello Welt!!";
+    }
+    res.json({msg});
+})
+
+router.route('/').post((req,res) => {
+    msg = `Hello ${req.body.name || "World"} from the request!`;
+    res.json(msg);
+})
+module.exports = router;
